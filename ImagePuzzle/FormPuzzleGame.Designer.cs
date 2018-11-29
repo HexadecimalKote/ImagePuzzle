733b2f4d943b89d86d3c3bda217816d2a39f6fc8
HexadecimalKote <mydeepnetwork@gmail.com>
2018-11-30 01:02:30 +0200

    Created a basic version of the game

--- ImagePuzzle/FormPuzzleGame.Designer.cs
+++ ImagePuzzle/FormPuzzleGame.Designer.cs
@@ -36,7 +36,11 @@
             this.Menu_Level_4 = new System.Windows.Forms.ToolStripMenuItem();
             this.Menu_Level_5 = new System.Windows.Forms.ToolStripMenuItem();
             this.Menu_Level_6 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ImagePanel = new System.Windows.Forms.Panel();
+            this.PB_ImagePart = new System.Windows.Forms.PictureBox();
             this.Menu_PuzzleGame.SuspendLayout();
+            this.ImagePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_ImagePart)).BeginInit();
             this.SuspendLayout();
             // 
             // Menu_PuzzleGame
@@ -46,7 +50,7 @@
             this.Menu_Level});
             this.Menu_PuzzleGame.Location = new System.Drawing.Point(0, 0);
             this.Menu_PuzzleGame.Name = "Menu_PuzzleGame";
-            this.Menu_PuzzleGame.Size = new System.Drawing.Size(747, 24);
+            this.Menu_PuzzleGame.Size = new System.Drawing.Size(792, 24);
             this.Menu_PuzzleGame.TabIndex = 0;
             this.Menu_PuzzleGame.Text = "menuStrip1";
             // 
@@ -56,6 +60,7 @@
             this.Menu_StartGame.Name = "Menu_StartGame";
             this.Menu_StartGame.Size = new System.Drawing.Size(102, 20);
             this.Menu_StartGame.Text = "Начать игру";
+            this.Menu_StartGame.Click += new System.EventHandler(this.Menu_StartGame_Click);
             // 
             // Menu_Level
             // 
@@ -72,40 +77,70 @@
             // Menu_Level_3
             // 
             this.Menu_Level_3.Name = "Menu_Level_3";
-            this.Menu_Level_3.Size = new System.Drawing.Size(180, 22);
+            this.Menu_Level_3.Size = new System.Drawing.Size(97, 22);
             this.Menu_Level_3.Text = "3 х 3";
+            this.Menu_Level_3.Click += new System.EventHandler(this.Menu_Level_3_Click);
             // 
             // Menu_Level_4
             // 
             this.Menu_Level_4.Name = "Menu_Level_4";
-            this.Menu_Level_4.Size = new System.Drawing.Size(180, 22);
+            this.Menu_Level_4.Size = new System.Drawing.Size(97, 22);
             this.Menu_Level_4.Text = "4 х 4";
+            this.Menu_Level_4.Click += new System.EventHandler(this.Menu_Level_4_Click);
             // 
             // Menu_Level_5
             // 
             this.Menu_Level_5.Name = "Menu_Level_5";
-            this.Menu_Level_5.Size = new System.Drawing.Size(180, 22);
+            this.Menu_Level_5.Size = new System.Drawing.Size(97, 22);
             this.Menu_Level_5.Text = "5 х 5";
+            this.Menu_Level_5.Click += new System.EventHandler(this.Menu_Level_5_Click);
             // 
             // Menu_Level_6
             // 
             this.Menu_Level_6.Name = "Menu_Level_6";
-            this.Menu_Level_6.Size = new System.Drawing.Size(180, 22);
+            this.Menu_Level_6.Size = new System.Drawing.Size(97, 22);
             this.Menu_Level_6.Text = "6 х 6";
+            this.Menu_Level_6.Click += new System.EventHandler(this.Menu_Level_6_Click);
+            // 
+            // ImagePanel
+            // 
+            this.ImagePanel.Controls.Add(this.PB_ImagePart);
+            this.ImagePanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ImagePanel.Location = new System.Drawing.Point(0, 24);
+            this.ImagePanel.Name = "ImagePanel";
+            this.ImagePanel.Size = new System.Drawing.Size(792, 423);
+            this.ImagePanel.TabIndex = 1;
+            // 
+            // PB_ImagePart
+            // 
+            this.PB_ImagePart.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PB_ImagePart.Location = new System.Drawing.Point(55, 105);
+            this.PB_ImagePart.Name = "PB_ImagePart";
+            this.PB_ImagePart.Size = new System.Drawing.Size(73, 65);
+            this.PB_ImagePart.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PB_ImagePart.TabIndex = 0;
+            this.PB_ImagePart.TabStop = false;
+            this.PB_ImagePart.Click += new System.EventHandler(this.PB_ImagePart_Click);
             // 
             // FormPuzzleGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(747, 438);
+            this.ClientSize = new System.Drawing.Size(792, 447);
+            this.Controls.Add(this.ImagePanel);
             this.Controls.Add(this.Menu_PuzzleGame);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.Menu_PuzzleGame;
+            this.MaximizeBox = false;
             this.Name = "FormPuzzleGame";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Puzzle Game";
+            this.Load += new System.EventHandler(this.FormPuzzleGame_Load);
             this.Menu_PuzzleGame.ResumeLayout(false);
             this.Menu_PuzzleGame.PerformLayout();
+            this.ImagePanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.PB_ImagePart)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -120,6 +155,8 @@
         private System.Windows.Forms.ToolStripMenuItem Menu_Level_4;
         private System.Windows.Forms.ToolStripMenuItem Menu_Level_5;
         private System.Windows.Forms.ToolStripMenuItem Menu_Level_6;
+        private System.Windows.Forms.Panel ImagePanel;
+        private System.Windows.Forms.PictureBox PB_ImagePart;
     }
 }
 
--- ImagePuzzle/FormPuzzleGame.cs
+++ ImagePuzzle/FormPuzzleGame.cs
@@ -1,20 +1,130 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ImagePuzzle
 {
     public partial class FormPuzzleGame : Form
     {
+        Game game;
+        int size = 3;
+        Image[,] img;
+        PictureBox[,] box;
+
         public FormPuzzleGame()
         {
             InitializeComponent();
         }
+
+        private void StartGame()
+        {
+            game = new Game(size);
+            CropImage();
+            InitPictures();
+            game.Start();
+            for (int i = 0; i < size * size * 10; i++)  
+                game.ShiftRandom();
+            RefreshImage();
+        }
+
+        private void CropImage()
+        {
+            img = new Image[size, size];
+            for (int x = 0; x < size; x++)
+                for (int y = 0; y < size; y++)
+                    img[x, y] = GetImagePart(x, y);
+        }
+
+        private Bitmap GetImagePart(int x, int y)
+        {
+            Bitmap bmp = Properties.Resources.PuzzleImage_2;
+            int w, h; 
+            w = bmp.Width / size;
+            h = bmp.Height / size;
+            Rectangle cropPart = new Rectangle(x * w, y * h, w, h);
+            Bitmap target = new Bitmap(w, h);
+            Graphics grp = Graphics.FromImage(target);
+            grp.DrawImage(bmp, new Rectangle(0, 0, target.Width, target.Height), cropPart, GraphicsUnit.Pixel);
+            return target;
+        }
+
+        private void InitPictures()
+        {
+            int w, h;
+            w = ImagePanel.Width / size;
+            h = ImagePanel.Height / size;
+
+            ImagePanel.Controls.Clear();
+            box = new PictureBox[size, size];
+            for (int x = 0; x < size; x++)
+                for (int y = 0; y < size; y++)
+                {
+                    PictureBox pb = new PictureBox();
+
+                    pb.BorderStyle = BorderStyle.FixedSingle;
+                    pb.Location = new Point(x * w, y * h);
+                    pb.Size = new Size(w, h);
+                    pb.SizeMode = PictureBoxSizeMode.StretchImage;
+                    pb.Click += new EventHandler(PB_ImagePart_Click);
+                    pb.Tag = y * size + x;
+
+                    ImagePanel.Controls.Add(pb);
+                    box[x, y] = pb;
+                }
+        }
+
+        private void RefreshImage()
+        {
+            for (int position = 0; position < size * size; position++)
+            {
+                int nr = game.GetNumber(position);
+                box[position % size, position / size].Visible = (nr >= 0);
+
+                if (nr >= 0)
+                    box[position % size, position / size].Image = img[nr % size, nr / size];
+            }
+        }
+
+        #region Events
+        private void FormPuzzleGame_Load(object sender, EventArgs e) => StartGame();
+
+        private void Menu_StartGame_Click(object sender, EventArgs e) => StartGame();
+
+        private void Menu_Level_3_Click(object sender, EventArgs e)
+        {
+            size = 3;
+            StartGame();
+        }
+
+        private void Menu_Level_4_Click(object sender, EventArgs e)
+        {
+            size = 4;
+            StartGame();
+        }
+
+        private void Menu_Level_5_Click(object sender, EventArgs e)
+        {
+            size = 5;
+            StartGame();
+        }
+
+        private void Menu_Level_6_Click(object sender, EventArgs e)
+        {
+            size = 6;
+            StartGame();
+        }
+
+        private void PB_ImagePart_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((PictureBox)sender).Tag);
+            game.Shift(position);
+            RefreshImage();
+            if (game.CheckNumbers())
+            {
+                MessageBox.Show("Вы победили!", "Поздравление");
+                StartGame();
+            }
+        }
+        #endregion
     }
 }
